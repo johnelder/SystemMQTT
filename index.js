3fa58198a5889f98b0a87d47f2da537cba57fde2
johnelder <john.elder.h@gmail.com>
2023-07-25 23:29:06 -0400

    Added error handling for connection

--- index.js
+++ index.js
@@ -8,62 +8,56 @@ console.log('Started')
 
 var connectString = broker.protocol + '://' + broker.host + ":" + broker.port
 const client = mqtt.connect(broker)
-//TODO add error handling for connection
 
-
-
-client.on('connect', function(packet, err) {
-    if (!err) {
-        console.log('Connected to ' + connectString)
-    } else {
-        console.error(err)
-    }
+client.on('connect', function(err) {
+    console.log('Connected to ' + connectString)
 })
 
 client.on('error', function(err) {
-    console.warn(err)
+    console.error(err)
 })
 
 
 
 setInterval(function() {
-    var hostname = osu.os.hostname();
-    getStats().then(systemData => {
-        options.logMessages ? console.log(systemData) : null
-        options.rootTopic = options.rootTopic ? options.rootTopic : 'SystemMQTT';
-        options.idTopic = options.hostTopic ? hostname : broker.clientId;
+    if (client.connected) {
+        var hostname = osu.os.hostname();
+        getStats().then(systemData => {
+            options.logMessages ? console.log(systemData) : null
+            options.rootTopic = options.rootTopic ? options.rootTopic : 'SystemMQTT';
+            options.idTopic = options.hostTopic ? hostname : broker.clientId;
 
-        if (options.messageType === 'json') {
-            client.publish(options.rootTopic + '/' + options.idTopic, JSON.stringify(systemData))
-            // client.publish('SystemMQTT', 'test')
-        } else {
+            if (options.messageType === 'json') {
+                client.publish(options.rootTopic + '/' + options.idTopic, JSON.stringify(systemData))
+                // client.publish('SystemMQTT', 'test')
+            } else {
 
-            for (const key in systemData) {
-                if (typeof(systemData[key]) == 'object') {
-                    for (const i in systemData[key]) {
-                        if (typeof(systemData[key][i]) == 'object') {
-                            for (const j in systemData[key][i]) {
-                                if (typeof(systemData[key][i][j]) == 'object') {
-                                    for (const k in systemData[key][i][j]) {
-                                        client.publish(options.rootTopic + '/' + options.idTopic + '/' + key + '/' + i + '/' + j + '/' + k, JSON.stringify(systemData[key][i][j][k]))
+                for (const key in systemData) {
+                    if (typeof(systemData[key]) == 'object') {
+                        for (const i in systemData[key]) {
+                            if (typeof(systemData[key][i]) == 'object') {
+                                for (const j in systemData[key][i]) {
+                                    if (typeof(systemData[key][i][j]) == 'object') {
+                                        for (const k in systemData[key][i][j]) {
+                                            client.publish(options.rootTopic + '/' + options.idTopic + '/' + key + '/' + i + '/' + j + '/' + k, JSON.stringify(systemData[key][i][j][k]))
+                                        }
+                                    } else {
+                                        client.publish(options.rootTopic + '/' + options.idTopic + '/' + key + '/' + i + '/' + j, JSON.stringify(systemData[key][i][j]))
                                     }
-                                } else {
-                                    client.publish(options.rootTopic + '/' + options.idTopic + '/' + key + '/' + i + '/' + j, JSON.stringify(systemData[key][i][j]))
                                 }
+                            } else {
+                                client.publish(options.rootTopic + '/' + options.idTopic + '/' + key + '/' + i, JSON.stringify(systemData[key][i]))
                             }
-                        } else {
-                            client.publish(options.rootTopic + '/' + options.idTopic + '/' + key + '/' + i, JSON.stringify(systemData[key][i]))
                         }
+                    } else {
+                        client.publish(options.rootTopic + '/' + options.idTopic + '/' + key, JSON.stringify(systemData[key]))
                     }
-                } else {
-                    client.publish(options.rootTopic + '/' + options.idTopic + '/' + key, JSON.stringify(systemData[key]))
+
                 }
 
             }
-
-        }
-    });
-
+        });
+    }
 
 
 
